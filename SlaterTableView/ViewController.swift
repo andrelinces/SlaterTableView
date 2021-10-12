fead0996e33b16ec10ad28d4a6c4d01e4c1fbed4
Andre Linces <andrelinces@yahoo.com.br>
2021-10-12 18:08:37 -0300

    Initial Commit

---
+++ SlaterTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlaterTableView
+//
+//  Created by Andre Linces on 12/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
